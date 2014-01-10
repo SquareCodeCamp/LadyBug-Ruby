e47d4d2fc9acc15222499ff485485bf5a384e911
Hannah Acheson-Field and Sean Sorrell <github+hacheson+rudle@squareup.com>
2014-01-10 15:06:51 -0800

    so security!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Session
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
   include Session
 
   protected
@@ -15,5 +15,4 @@ class ApplicationController < ActionController::Base
   	end
 
   end
-
 end
