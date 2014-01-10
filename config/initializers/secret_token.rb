efaae370a7941512817cd5e733c2534f18384e74
Ed Ruder and Sean Sorrell <github+ed+ssorrell@squareup.com>
2014-01-10 10:02:14 -0800

    FIRST COMMIT WOOO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ladybug::Application.config.secret_key_base = '960ae77e425586aa044a3e2e1393cc4d538356ac26a35cfa958dbe5e8027794db82939e2b608fbca90f0b16a3af3e0dba63e3761d6efdbd32265ac40d3ee7b77'
