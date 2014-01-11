cf072355f128588a09a417d69c78abc0e83e5dbe
Ed Ruder and Sean Sorrell <github+ed+ssorrell@squareup.com>
2014-01-10 16:50:01 -0800

    login works on backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def require_login
-
   	unless current_user
   		redirect_to login_path
   		return false
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
